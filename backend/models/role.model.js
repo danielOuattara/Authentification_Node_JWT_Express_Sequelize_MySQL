57df63f2622f4597a3b257e0aeaa17cda0161446
daniel ouattara <daniel.ouattara@gmx.com>
2021-02-25 22:58:24 +0100

    project commit

---
+++ backend/models/role.model.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, Sequelize) => {
+  const Role = sequelize.define("roles", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true
+    },
+    name: {
+      type: Sequelize.STRING
+    }
+  });
+
+  return Role;
+};
+
+
+//  Sequelize model 'Role' represents roles table in MySQL database.
